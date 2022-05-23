67b1495eee5965f5b28fc50a7e7bda8caed9fb14
studstone <nikolas2996@gmail.com>
2022-05-23 22:32:15 +0300

    add setting style.css and fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import { React } from "react";
+import { createGlobalStyle } from "styled-components";
+
+const ClobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    background-color: f0f0f0;
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    color: black;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  h1,
+  h2,
+  h3{
+    font-family: 'Pacifico', cursive;
+    padding: 0;
+    margin: 0;
+  }
+
+  p{
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>привет</h1>
+    <div>
+      <ClobalStyle />
+      <div className="App">
+        <h1>привет</h1>
+      </div>
     </div>
   );
 }
