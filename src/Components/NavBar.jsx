ed00b34a3ed8807aab0640c2d8d0512a6c33df3c
studstone <nikolas2996@gmail.com>
2022-05-29 19:33:00 +0300

    add style in button

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -37,7 +37,6 @@ const HeaderButton = styled.button`
     display: flex;
     flex-direction: column;
     align-items: center;
-    cursor: pointer;
     border: none;
     background-color: transparent;
     color: inherit;
